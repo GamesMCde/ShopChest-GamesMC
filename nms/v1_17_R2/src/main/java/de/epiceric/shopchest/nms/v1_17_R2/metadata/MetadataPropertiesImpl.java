d7460cac05e7ae80ee01308b830c8ff94b830232
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2022-12-31 18:20:21 +0100

    Implement v1_17_R2

---
+++ nms/v1_17_R2/src/main/java/de/epiceric/shopchest/nms/v1_17_R2/metadata/MetadataPropertiesImpl.java
@@ -0,0 +1,21 @@
+package de.epiceric.shopchest.nms.v1_17_R2.metadata;
+
+import de.epiceric.shopchest.nms.metadata.MetadataProperties;
+
+public class MetadataPropertiesImpl implements MetadataProperties {
+
+    @Override
+    public Entity entity() {
+        return new EntityMetadataProperties();
+    }
+
+    @Override
+    public ArmorStand armorStand() {
+        return new ArmorStandMetadataProperties();
+    }
+
+    @Override
+    public Item item() {
+        return new ItemMetadataProperties();
+    }
+}
